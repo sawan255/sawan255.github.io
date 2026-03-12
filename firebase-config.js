6610b2eaa936a818d7a27b74eb28d4ca80996d2c
sawan255 <sawan5555555555@gmail.com>
2026-03-12 18:42:45 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIZf3eVM2xL3Q4FwpGRXIFAc4_qCIOc14",
-  authDomain: "utilitydamn.firebaseapp.com",
-  projectId: "utilitydamn",
-  storageBucket: "utilitydamn.firebasestorage.app",
-  messagingSenderId: "109181511894",
-  appId: "1:109181511894:web:14b61d527a8ed446c3c177",
-  measurementId: "G-TVG2FLCK28",
+  apiKey: "AIzaSyDfNjK_idKX8zAE23khW9Dlb9xFJ3sbVO4",
+  authDomain: "kingcobrasure.firebaseapp.com",
+  projectId: "kingcobrasure",
+  storageBucket: "kingcobrasure.firebasestorage.app",
+  messagingSenderId: "975421123123",
+  appId: "1:975421123123:web:43d3f65983c914abfe1146",
+  measurementId: "G-5P634N59P7",
 };
 
 const app = initializeApp(firebaseConfig);
